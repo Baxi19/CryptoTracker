dfe62044800edc624d3de9a722d44891b2f354d1
Baxi19 <randald1991@gmail.com>
2021-02-25 22:19:47 -0600

    :fire: Navegación entre pantallas

--- src/components/coins/CoinsScreen.js
+++ src/components/coins/CoinsScreen.js
@@ -1,15 +1,51 @@
 import React, { Component } from 'react';
-import { View, Text }from 'react-native';
+import { View, Text, Pressable, StyleSheet }from 'react-native';
 
 
 class CoinsScreen extends Component {
+    
+    handlePress = () => {
+        {/*console.log("Go to detail", this.props);*/}
+        this.props.navigation.navigate("CoinDetail");
+    }
+    
     render() {
         return (
-            <View>
-                <Text>Coins Screen</Text>
+            <View style={styles.container}>
+                <Text style={styles.title}>Coins Screen</Text>
+                
+                <Pressable style={styles.btn} onPress={this.handlePress}> 
+                    <Text style={styles.btnText}>Go to Detail</Text>
+                </Pressable>
+
             </View>
         );
     }
 }
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "pink",
+    },
+
+    title: {
+        color: "#fff",
+        textAlign: "center" 
+    },
+
+    btn: {
+        padding: 8,
+        backgroundColor: "green",
+        borderRadius: 8,
+        margin: 16
+    },
+
+    btnText: {
+       color: "#fff",
+       textAlign: "center" 
+    }
+});
 
 export default CoinsScreen;
--- src/components/coins/CoinsStack.js
+++ src/components/coins/CoinsStack.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import CoinsScreen from './CoinsScreen';
 import { createStackNavigator } from '@react-navigation/stack';
+import CoinDetailScreen from './CoinsDetailScreen';
 
 const Stack = createStackNavigator();
 
 const CoinsStack = () => {
     return (
         <Stack.Navigator>
-            <Stack.Screen name="Coins" component={CoinsScreen}/>
+
+            <Stack.Screen 
+                name="Coins" 
+                component={CoinsScreen}
+            />
+            
+            <Stack.Screen 
+                name="CoinDetail" 
+                component={CoinDetailScreen} 
+            />
+
         </Stack.Navigator>
     );
 }
