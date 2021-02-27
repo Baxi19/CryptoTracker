6b2386aa930f560576b07c733557581668dd76ab
Baxi19 <randald1991@gmail.com>
2021-02-27 10:04:18 -0600

    :octocat: Agregando tabs de navegación :fire:

--- App.js
+++ App.js
@@ -1,11 +1,37 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import CoinsStack from 'CryptoTracker/src/components/coins/CoinsStack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import { Image } from 'react-native';
+import Colors from 'CryptoTracker/src/res/colors';
+
+const Tabs = createBottomTabNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <CoinsStack/>
+
+      <Tabs.Navigator
+        tabBarOptions={{
+          tintColor: "#fefefe",
+          style: {
+          backgroundColor: Colors.blackPearl
+          }
+        }}  
+      >
+        <Tabs.Screen
+          name="Coins"
+          component={CoinsStack}
+          options={{
+              tabBarIcon: ({size, color}) => 
+                <Image 
+                  style={{tintColor: color, width: size, height: size }}
+                  source={require('CryptoTracker/src/assets/bank.png')}
+                />
+          }}
+        />
+      </Tabs.Navigator>
+
     </NavigationContainer>
   );
 };
--- README.md
+++ README.md
@@ -8,6 +8,8 @@ npm install @react-navigation/native @react-navigation/stack
 
 npm install react-native-reanimated react-native-gesture-handler react-native-screens react-native-safe-area-context @react-native-community/masked-view
 
+npm install @react-navigation/bottom-tabs
+
 #### Run App
 
 npx react-native start
